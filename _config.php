a81ff270869cd9cbcf8ac68a152f1adb083447b3
Colin Tucker <colin@praxis.net.au>
2017-10-08 10:15:23 +1100

    Updated build dependencies

--- _config.php
+++ _config.php
@@ -14,13 +14,3 @@
  * @license https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
  * @link https://github.com/praxisnetau/silverware
  */
-
-// Define Module Constants:
-
-if (!defined('SILVERWARE_DIR')) {
-    define('SILVERWARE_DIR', basename(__DIR__));
-}
-
-if (!defined('SILVERWARE_PATH')) {
-    define('SILVERWARE_PATH', realpath(__DIR__));
-}
